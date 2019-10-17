f1714ca7ee5fba25cc1b129d6565e636071cab6b
Dinar Shakurov <dinar5d33@gmail.com>
2019-10-17 19:48:13 +0300

    first servlet

--- src/main/java/ru/itis/web_project/servlets/LoginServlet.java
+++ src/main/java/ru/itis/web_project/servlets/LoginServlet.java
@@ -1,15 +1,17 @@
 package ru.itis.web_project.servlets;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@WebServlet("/login")
 public class LoginServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+        req.getRequestDispatcher("/jsp/login.jsp").forward(req, resp);
     }
 
     @Override
--- src/main/webapp/jsp/registration.jsp
+++ src/main/webapp/jsp/registration.jsp
@@ -1,5 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html lang="кг">
 <head>
     <meta charset="UTF-8">
     <title>Registration</title>
@@ -12,7 +12,7 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css"
           integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="../css/style.css">
 </head>
 <body>
 
